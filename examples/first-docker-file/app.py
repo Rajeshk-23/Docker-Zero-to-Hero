2086758af33752b12e5cf3ab28f932b2072b1f4c
KURNUTHALA RAJESH KUMAR <krazesh7@gmail.com>
2025-12-04 15:04:08 +0530

    Convert script to Flask application with routes

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Raazu 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
